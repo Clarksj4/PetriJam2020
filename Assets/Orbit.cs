cc4c80c1ee768013f0d822be476e8a63b00f4c47
Aleksis Kavalieris <aleksis@runawayplay.com>
2020-12-06 11:31:30 +1300

    some animation stuff

---
+++ Assets/Orbit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    float angle = 0;
+    float speed = (2 * Mathf.PI) / 20f;
+    float radius = 3;
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+        Vector3 pos = new Vector3 (Mathf.Cos(angle) * radius, 0, Mathf.Sin(angle) * radius);
+        transform.localPosition = pos;
+    }
+}
